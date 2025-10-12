c241a9ae1fe1f5587ea0daf8d5349077b573b9fd
Joel <joelpimentel1827@gmail.com>
2025-10-11 21:17:48 -0500

    se realizo el problema 4

--- Laboratorio-3/Problema4.cs
+++ Laboratorio-3/Problema4.cs
@@ -12,9 +12,83 @@ namespace Laboratorio_3
 {
     public partial class Problema4 : Form
     {
+        List<double> ventas = new List<double>();
         public Problema4()
         {
             InitializeComponent();
+            txtCant.KeyDown += TxtCant_KeyDown;
+        }
+        private void TxtCant_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                AgregarVenta();
+                e.SuppressKeyPress = true;
+            }
+        }
+        private void AgregarVenta()
+        {
+            if (ventas.Count >= NumVen.Value)
+            {
+                MessageBox.Show("Ya estan tdas las Ventas seleccionadas", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            if (double.TryParse(txtCant.Text, out double importe))
+            {
+                ventas.Add(importe);
+                ltsCantidad.Items.Add(importe);
+                txtCant.Clear();
+                txtCant.Focus();
+                if (ventas.Count == NumVen.Value)
+                {
+                    MessageBox.Show("Has ingresado todas las ventas , ahora Importe", "informacon", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Ingrese un número válido", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void bntImporte_Click(object sender, EventArgs e)
+        {
+            if (ventas.Count == 0)
+            {
+                MessageBox.Show("No hay importes ingresados.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            List<double> importesRa = ventas.Where(v => v >= 75 && v <= 215).ToList();
+
+            if (importesRa.Count == 0)
+            {
+                MessageBox.Show("No hay importes dentro del rango de $75 a $215.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            DgbCompleto.DataSource = ventas.Where(v => v >= 75 && v <= 215).Select(v => new { Importe = v }).ToList();
+        }
+        private void btnOrdenar_Click(object sender, EventArgs e)
+        {
+
+            List<double> importesOrdenados = ventas.Where(v => v >= 75 && v <= 215).OrderBy(v => v).ToList();
+
+            if (importesOrdenados.Count == 0)
+            {
+                MessageBox.Show("No hay importes dentro del rango de $75 a $215.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            DgbOrdenado.DataSource = importesOrdenados.Select(v => new { Importe = v }).ToList();
+        }
+
+        private void btnRefrescar_Click(object sender, EventArgs e)
+        {
+            ventas.Clear();
+            ltsCantidad.Items.Clear();
+            DgbCompleto.DataSource = null;
+            DgbOrdenado.DataSource = null;
+            txtCant.Clear();
+            NumVen.Value = 0;
+            txtCant.Focus();
+            MessageBox.Show("El formulario ha sido reiniciado.", "Refrescar", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
     }
-}
+    }
+
